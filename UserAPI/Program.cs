58d9473b3d8f5f8f59b05dc8dd637b1fc55f7a09
diamond <aaron.diamond2@yahoo.co.nz>
2026-03-02 06:31:05 +1300

    change url in cors

--- UserAPI/Program.cs
+++ UserAPI/Program.cs
@@ -18,7 +18,7 @@ namespace UserAPI
                 options.AddPolicy("AllowSpecificOrigin",
                     policy =>
                     {
-                        policy.WithOrigins("http://localhost:5173") // Allow your frontend's URL
+                        policy.WithOrigins("https://phase0.net") // Allow your frontend's URL
                               .AllowCredentials()  // Allow credentials (cookies, authorization headers)
                               .AllowAnyHeader()    // Allow any headers
                               .AllowAnyMethod();   // Allow any HTTP method
